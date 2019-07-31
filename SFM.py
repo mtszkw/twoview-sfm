2c0b1978920ee0a8eb71b6b078396ae2d582ad12
mtszkw <mtszkw@protonmail.com>
2019-07-31 15:18:03 +0200

    Match only (i, i+1) images instead of all-to-all

--- SFM.py
+++ SFM.py
@@ -12,58 +12,59 @@ np.set_printoptions(suppress=True)
 dataReader = DataReader(datasetDirectory="data/temple/")
 
 matcher = cv2.BFMatcher()
-extractor = cv2.xfeatures2d.SIFT_create()
-K, _, _ = dataReader.readCameraParams(idx=1)
+sift = cv2.xfeatures2d.SIFT_create()
+K, _, _ = dataReader.readCameraParams()
 print(f"\nIntrinsic parameters matrix:\n{K}")
 
 cloudPoints, cloudColors = [], []
-imagesCount = 30 # dataReader.getImagesCount() - 1
+imagesCount = dataReader.getImagesCount() - 1
 
-for frameIdx1 in range(1, imagesCount):
+for frameIdx in range(2, imagesCount):
+    prevFrame = dataReader.readFrame(frameIdx-1, convertToRGB=True)
+    currFrame = dataReader.readFrame(frameIdx, convertToRGB=True)
+    prevKeypts, prevDescr = sift.detectAndCompute(prevFrame, mask=None)
+    currKeypts, currDescr = sift.detectAndCompute(currFrame, mask=None)
 
-    prevFrame = dataReader.readFrame(frameIdx1, convertToRGB=True)
-    currKeypts, prevDescr = extractor.detectAndCompute(prevFrame, mask=None)
+    matches = matcher.knnMatch(queryDescriptors=prevDescr, trainDescriptors=currDescr, k=2)
+    print(f"\nMatching: Found {len(matches)} matches betweens frames ({frameIdx-1}, {frameIdx}).")
 
-    for frameIdx2 in range(frameIdx1+1, imagesCount):
-        
-        currFrame = dataReader.readFrame(frameIdx2, convertToRGB=True)
-        prevKeypts, currDescr = extractor.detectAndCompute(currFrame, mask=None)
+    goodMatches = [m for m, n in matches if m.distance < 0.8*n.distance]        
+    prevPts = [prevKeypts[m.queryIdx].pt for m in goodMatches]
+    currPts = [currKeypts[m.trainIdx].pt for m in goodMatches]
+    print(f"Lowe-Ratio filter: {len(goodMatches)} matches left (ratio = 0.8).")
 
-        matches = matcher.knnMatch(queryDescriptors=prevDescr, trainDescriptors=currDescr, k=2)
-        print(f"\nMatching: Found {len(matches)} matches betweens frames ({frameIdx1}, {frameIdx2}).")
+    prevPts = np.array(prevPts)
+    currPts = np.array(currPts)
+    E, inlierMask = cv2.findEssentialMat(currPts, prevPts, K, cv2.RANSAC, 0.99, 1.0, None)
+    _, R, t, inlierMask = cv2.recoverPose(E, currPts, prevPts, K, mask=inlierMask)
+    prevPts = np.array([pt for (idx, pt) in enumerate(prevPts) if inlierMask[idx] == 1])
+    currPts = np.array([pt for (idx, pt) in enumerate(currPts) if inlierMask[idx] == 1])
+    print(f"RANSAC filter: {len(prevPts)} keypoints left after applying inlier mask.")
+    print("Rotation R =", R.flatten())
+    print("Translation t =", t.flatten())
+    print("Essential matrix:", E.flatten())
 
-        goodMatches = [m for m, n in matches if m.distance < 0.8*n.distance]        
-        prevPts = [currKeypts[m.queryIdx].pt for m in goodMatches]
-        currPts = [prevKeypts[m.trainIdx].pt for m in goodMatches]
-        print(f"Lowe-Ratio filter: {len(goodMatches)} matches left (ratio = 0.8).")
+    if len(prevPts) == 0:
+        continue
 
-        # Filtering outliers using fundamental matrix
-        _, inlierMask = cv2.findFundamentalMat(np.array(prevPts), np.array(currPts), cv2.FM_RANSAC, 1, 0.99)
-        prevPts = np.array([pt for (idx, pt) in enumerate(prevPts) if inlierMask[idx] == 1])
-        currPts = np.array([pt for (idx, pt) in enumerate(currPts) if inlierMask[idx] == 1])
-        print(f"RANSAC filter: {len(prevPts)} keypoints left after applying inlier mask.")
-
-        E, mask = cv2.findEssentialMat(currPts, prevPts, K, cv2.RANSAC, 0.99, 1.0, None)
-        _, R, t, mask = cv2.recoverPose(E, currPts, prevPts, K)
-        print("Rotation R =\n", R)
-        print("Translation t =", t.flatten())
-        print("Essential matrix:\n", E)
-
-        P1 = np.eye(3,4)
-        P2 = np.hstack((R, t))
-        points3D = cv2.triangulatePoints(P1, P2, np.transpose(prevPts), np.transpose(currPts))
-        points3D = [[x/w, y/w, z/w] for [x, y, z, w] in np.transpose(points3D)] # Convert to heterogeneous
-        
-        cloudPoints += points3D
-        cloudColors += [prevFrame[int(pt[1]), int(pt[0])] for pt in prevPts]
+    P1 = np.eye(3, 4)
+    P2 = np.hstack((R, t))
+    focal, cx, cy = K[0, 0], K[0, 2], K[1, 2]
+    normPrevPts = np.array([ [(p[0]-cx)/focal, (p[1]-cy)/focal] for p in prevPts])
+    normCurrPts = np.array([ [(p[0]-cx)/focal, (p[1]-cy)/focal] for p in currPts])
+    points3D = cv2.triangulatePoints(P1, P2, np.transpose(normPrevPts), np.transpose(normCurrPts))
+    points3D = [[x/w, y/w, z/w] for [x, y, z, w] in np.transpose(points3D)] # Convert to heterogeneous
+    
+    cloudPoints += points3D
+    cloudColors += [prevFrame[int(pt[1]), int(pt[0])] for pt in prevPts]
 
 print(f"\nReconstructed total {len(cloudPoints)} points from {imagesCount} frames.")
 
 vertexes = [ (p[0], p[1], p[2], c[0], c[1], c[2]) for p, c in zip(cloudPoints, cloudColors)]
+vertexes = [ v for v in vertexes if v[2] >= 0 ] # Discard negative z
 dtypes = [('x', 'f8'), ('y', 'f8'), ('z', 'f8'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')]
 array = np.array(vertexes, dtype=dtypes)
 element = plyfile.PlyElement.describe(array, "vertex")
 plyfile.PlyData([element]).write("sfm_cloud.ply")
 
-# print(f"\nSample 3D points with colors:\n", np.hstack((cloudPoints, cloudColors))[:10])
-# PyntCloud(pd.DataFrame(data=np.hstack((cloudPoints, cloudColors)), columns=["x", "y", "z", "red", "green", "blue"])).to_file("sfm_cloud.ply")
+print(f"\nSample 3D points with colors:\n", vertexes[:10])
